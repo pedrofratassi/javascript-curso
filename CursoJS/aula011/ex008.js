ce186f78adfd16eb3b3ec7c20aae5e9d8c32171d
Pedro Fratassi <pedrofratassi2@gmail.com>
2022-04-25 03:45:48 -0300

    Criação da Aula 011

---
+++ CursoJS/aula011/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 59
+
+console.log(`A velocidade do seu é ${velocidade}Km/h`)
+
+if(velocidade > 60){
+    console.log(`Você ultrapassou a velocidade permitida, por isso será Multado!`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança`)
---
+++ CursoJS/aula011/ex009.js
@@ -0,0 +1,9 @@
+var nacao = "Brasil"
+
+console.log(`Vivendo no ${nacao}`)
+
+if(nacao == "Brasil"){
+    console.log(`Brasileiro`)
+} else {
+    console.log(`Estrangeiro`)
+}
